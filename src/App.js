0562220918af6c6b1ea1011168bc1606ff407696
kushalnigam0604 <nigamkushal0@gmail.com>
2023-03-19 23:06:54 +0530

    Change the app functionality and UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState } from "react";
+import React from "react";
 import Header from "./components/Header/Header";
 import Balance from "./components/Balance/Balance";
 import IncomeExpenses from "./components/IncomeExpenses/IncomeExpenses";
